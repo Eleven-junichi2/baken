780b969ab6134915a0b72e15fc3f24ddeb8a0e70
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2025-03-24 16:27:42 +0900

    use selenium

--- baken/__main__.py
+++ baken/__main__.py
@@ -2,8 +2,9 @@ from dataclasses import dataclass, field
 from enum import Enum, auto, StrEnum
 import re
 
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import requests
 
 
 class TrackSurface(StrEnum):
@@ -32,6 +33,39 @@ class RaceInfo:
     track_condition: TrackCondition | None = None
     weather: Weather | None = None
     entries: list["HorseEntry"] = field(default_factory=list)
+
+    # 勝率を計算するメソッド
+    def calc_win_rate(self) -> float:
+        # 単勝人気順に勝率が定義されたタプル
+        win_rate_values_by_popularity = (
+            0.328,
+            0.19,
+            0.133,
+            0.092,
+            0.075,
+            0.055,
+            0.038,
+            0.031,
+            0.022,
+            0.012,
+            0.013,
+            0.009,
+            0.007,
+            0.0001,
+            0.002,
+            0.001,
+            0.0001,
+            0.0001,
+        )
+        sorted_entries: tuple[HorseEntry] = tuple(
+            sorted(self.entries, key=lambda x: x.odds)
+        )
+        for popularity, (entry, win_rate_by_popularity) in enumerate(
+            zip(sorted_entries, win_rate_values_by_popularity)
+        ):
+            print(
+                f"{popularity}番人気の勝率={win_rate_by_popularity} {entry.horse.name} オッズ：{entry.odds} 期待値={entry.odds * win_rate_by_popularity}"
+            )
 
 
 class TrainingCenter(StrEnum):
@@ -64,29 +98,43 @@ class HorseEntry:
 
 if __name__ == "__main__":
     race_info = RaceInfo()
+
     netkeiba_shutuba_url = input("netkeiba raceid：")
     if netkeiba_shutuba_url == "":
         netkeiba_shutuba_url = (
             r"https://race.netkeiba.com/race/shutuba.html?race_id=202509010811"
         )
-    netkeiba_odds_url = input("netkeiba odds：")
-    if netkeiba_odds_url == "":
-        netkeiba_odds_url = (
-            r"https://race.netkeiba.com/odds/index.html?race_id=202509010811"
-        )
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    response = requests.get(
-        netkeiba_shutuba_url,
-        headers=headers,
+    # netkeiba_odds_url = input("netkeiba odds：")
+    # if netkeiba_odds_url == "":
+    #     netkeiba_odds_url = (
+    #         r"https://race.netkeiba.com/odds/index.html?race_id=202509010811"
+    #     )
+
+    # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+
+    # response = requests.get(
+    #     netkeiba_shutuba_url,
+    #     headers=headers,
+    # )
+    # response.encoding = response.apparent_encoding
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    driver.get(netkeiba_shutuba_url)
+    shutsuba_html_interface = BeautifulSoup(
+        driver.page_source.encode("utf-8"), "html.parser"
     )
-    response.encoding = response.apparent_encoding
-    shutsuba_html_interface = BeautifulSoup(response.text, "html.parser")
-    response = requests.get(
-        netkeiba_odds_url,
-        headers=headers,
-    )
-    response.encoding = response.apparent_encoding
-    odds_html_interface = BeautifulSoup(response.text, "html.parser")
+    driver.quit()
+
+    # driver = webdriver.Firefox()
+    # driver.implicitly_wait(10)
+    # driver.get(netkeiba_odds_url)
+    # odds_html_interface = BeautifulSoup(driver.page_source.encode("utf-8"), "html.parser")
+    # response = requests.get(
+    #     netkeiba_odds_url,
+    #     headers=headers,
+    # )
+    # response.encoding = response.apparent_encoding
+    # odds_html_interface = BeautifulSoup(response.text, "html.parser")
     surface_and_distance = (
         shutsuba_html_interface.find("div", class_="RaceData01")
         .find("span")
@@ -97,10 +145,12 @@ if __name__ == "__main__":
     race_info.distance_in_meters = int(surface_and_distance[1:-1])
     print(vars(race_info))
     entry_table = shutsuba_html_interface.select("table.Shutuba_Table")
-    tansyo_fukusyo_table = odds_html_interface.select("table#Ninki")
-    print(tansyo_fukusyo_table)
+    # tansyo_fukusyo_table = odds_html_interface.select("table#Ninki")
+    # print(tansyo_fukusyo_table)
     for row in entry_table[0].select("tr.HorseList"):
-        post_position = row.find("td", class_=re.compile(r"Umaban[1-8]")).get_text()
+        post_position = int(
+            row.find("td", class_=re.compile(r"Umaban[1-8]")).get_text()
+        )
         horse_name = row.find(class_="HorseName").get_text()
         age = int(row.find(class_="Barei").get_text()[1:])
         jockey = row.find(class_="Jockey").get_text().strip()
@@ -109,6 +159,7 @@ if __name__ == "__main__":
         traning_center, trainer = trainer[:2], trainer[2:]
         weight, weight_change = row.find(class_="Weight").get_text().strip().split("(")
         weight_change = weight_change.rstrip(")")
+        odds = float(row.find(id=re.compile(r"^odds")).get_text().strip())
         print(
             post_position,
             horse_name,
@@ -118,7 +169,16 @@ if __name__ == "__main__":
             trainer,
             weight,
             weight_change,
+            odds,
         )
+        race_info.entries.append(
+            HorseEntry(
+                horse=Horse(name=horse_name, age=age),
+                post_position=post_position,
+                odds=odds,
+            )
+        )
+    race_info.calc_win_rate()
 
     # for data in horse_datas:
     #     race_info.entries.append(
